9fe1615231a0476244af782d609da001607743c6
mkaminsk95 <m.kaminsk95@gmail.com>
2024-11-27 10:18:31 +0100

    create base abstract class

---
+++ Src/Animals/Animal.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Animals;
+
+abstract class Animal
+{
+    public function __construct(
+        protected string $name,
+        protected string $species
+    ) {}
+
+    public function __toString(): string
+    {
+        return $this->species . ' ' . $this->name;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
