5b6af00049d35ae34a90373663d45be0453dc8f1
michalkaminski <m.kaminski@clawrock.com>
2024-11-27 15:02:17 +0100

    refactor code

--- Src/i18n/load_translations.php
+++ Src/i18n/load_translations.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 const DEFAULT_LANGUAGE = 'pl';
 
-function __($key, $language = DEFAULT_LANGUAGE) {
+function __(string $key, string $language = DEFAULT_LANGUAGE): string
+{
     $translations = include 'translations.php';
     return $translations[$language][$key] ?? $key;
 }
